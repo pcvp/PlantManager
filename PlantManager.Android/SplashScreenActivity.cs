1c6cf6909b8eb939ca69c6bb690b6abb48ffc184
Paulo Vieira <paulovieira@Paulos-MacBook-Pro.local>
2021-04-23 07:55:17 -0700

    SplashScreen

---
+++ PlantManager.Android/SplashScreenActivity.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace PlantManager.Droid
+{
+    
+    [Activity(Label = "Splash",Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed() { }
+     
+    }
+}
