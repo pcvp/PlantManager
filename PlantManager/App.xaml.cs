945457754977a98160bcdf77cbefeb03ba7f143a
Paulo Vieira <paulovieira@Paulos-MacBook-Pro.local>
2021-04-23 18:40:14 -0700

    Boas Vindas Page

--- PlantManager/App.xaml.cs
+++ PlantManager/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PlantManager.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace PlantManager
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new BoasVindasPage();
         }
 
         protected override void OnStart()
--- PlantManager/AssemblyInfo.cs
+++ PlantManager/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Jost.ttf")]
+[assembly: ExportFont("JostSemiBold.ttf")]
+[assembly: ExportFont("JostExtraBold.ttf")]
